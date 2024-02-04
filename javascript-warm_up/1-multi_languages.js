6909bbe07faef40ea4b8c8568874c428fb3d4842
AngeGitego <144104486+AngeGitego@users.noreply.github.com>
2024-02-04 03:15:12 -0800

    Update 1-multi_languages.js

--- javascript-warm_up/1-multi_languages.js
+++ javascript-warm_up/1-multi_languages.js
@@ -1,4 +1,9 @@
-#!/usr/bin/node
-console.log('C is fun');
-console.log('Python is cool');
-console.log('JavaScript is amazing');
+// Using let to declare variables
+let line1 = "C is fun";
+let line2 = "Python is cool";
+let line3 = "JavaScript is amazing";
+
+// Printing lines using console.log(...)
+console.log(line1);
+console.log(line2);
+console.log(line3);
