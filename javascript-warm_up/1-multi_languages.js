29c24a8997e809a822238d023bdfca406ec2bde5
AngeGitego <144104486+AngeGitego@users.noreply.github.com>
2024-02-04 03:31:42 -0800

    Update 1-multi_languages.js

--- javascript-warm_up/1-multi_languages.js
+++ javascript-warm_up/1-multi_languages.js
@@ -1,10 +1,5 @@
-
 #!/usr/bin/node
-let line1 = "C is fun";
-let line2 = "Python is cool";
-let line3 = "JavaScript is amazing";
 
-// Printing lines using console.log(...)
-console.log(line1);
-console.log(line2);
-console.log(line3);
+console.log('C is fun');
+console.log('Python is cool');
+console.log('JavaScript is amazing');
