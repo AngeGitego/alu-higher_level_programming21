a9f4e6bd398b9fc451a00451e11e3e5c74ec96c3
AngeGitego <144104486+AngeGitego@users.noreply.github.com>
2024-02-04 16:51:04 -0800

    Update 0-readme.js

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,5 +1,20 @@
 #!/usr/bin/node
 const fs = require('fs');
-fs.readFile(process.argv[2], 'utf8', function (error, content) {
-  console.log(error || content);
+
+// Check if the file path is provided
+if (process.argv.length !== 3) {
+    console.error('Usage: node 0-readme.js <file_path>');
+    process.exit(1);
+}
+
+// Get the file path from command line argument
+const filePath = process.argv[2];
+
+// Read the content of the file
+fs.readFile(filePath, 'utf-8', (err, data) => {
+    if (err) {
+        console.error(err);
+    } else {
+        console.log(data);
+    }
 });
