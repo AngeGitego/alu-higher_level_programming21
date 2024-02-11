8b728a48196f926f67b75c2f953ca4bf114d4ad1
AngeGitego <144104486+AngeGitego@users.noreply.github.com>
2024-02-11 13:30:20 -0800

    Update 4-starwars_count.js

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -1,23 +1,23 @@
 #!/usr/bin/node
-
-// Import the 'request' module
 const request = require('request');
 
-// Use the 'request' module to perform an HTTP GET request to the URL
 request(process.argv[2], function (error, response, body) {
-  // Check if there was no error during the HTTP request.
-  if (!error) {
-    // parse the JSON data and extract the "results" array
-    const results = JSON.parse(body).results;
-    // Use the 'reduce()' method to iterate through the movies in the 'results' array.
-    console.log(results.reduce((count, movie) => {
-      // check if there is a character with ID 18 ('/18/') in the 'characters' array.
-      return movie.characters.find((character) => character.endsWith('/18/'))
-        // If a character with ID 18 is found, increment the count by 1.
-        ? count + 1
-        // Otherwise, keep the count unchanged.
-        : count;
-      // The 'reduce()' method starts with an initial value of 0 ('0' at the end).
-    }, 0));
+  if (error) {
+    console.log(error);
+  } else {
+    const data = JSON.parse(body);
+    const movieCount = data.results.length;
+    let finalResult = 0;
+    // const comString = 'https://swapi-api.hbtn.io/api/people/18/';
+    for (let i = 0; i < movieCount; i++) {
+      const numChars = data.results[i].characters.length;
+      for (let j = 0; j < numChars; j++) {
+        const comString0 = data.results[i].characters[j];
+        if (comString0.slice(-4, -1) === '/18') {
+          finalResult += 1;
+        }
+      }
+    }
+    console.log(finalResult);
   }
 });
