446fbf2b5ef75af2da90c4f9579282997b86a329
AngeGitego <144104486+AngeGitego@users.noreply.github.com>
2024-02-11 13:28:02 -0800

    Update 0-readme.js

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,5 +1,13 @@
 #!/usr/bin/node
+
+const args = process.argv.slice(2);
 const fs = require('fs');
-fs.readFile(process.argv[2], 'utf8', function (error, content) {
-  console.log(error || content);
+
+fs.readFile(args[0], 'utf-8', (error, data) => {
+  if (error) {
+    console.error(error);
+    console.error(error);
+  } else {
+    console.log(data);
+  }
 });
