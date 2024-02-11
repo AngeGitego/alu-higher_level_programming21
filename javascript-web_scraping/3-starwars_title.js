5a1f736b45106039897fa423601c2ec4053c51a5
AngeGitego <144104486+AngeGitego@users.noreply.github.com>
2024-02-11 13:29:48 -0800

    Update 3-starwars_title.js

--- javascript-web_scraping/3-starwars_title.js
+++ javascript-web_scraping/3-starwars_title.js
@@ -1,14 +1,11 @@
 #!/usr/bin/node
-
-// Import the 'request' module.
 const request = require('request');
+const args = process.argv;
 
-// Construct the URL for the specific Star Wars film
-const url = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2];
-
-// Use the 'request' module to perform an HTTP GET request to the constructed URL.
-request(url, function (error, response, body) {
-  // log title if successful, log error if not.
-  console.log(error || JSON.parse(body).title);
+request(`https://swapi-api.hbtn.io/api/films/${args[2]}`, function (error, response, body) {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log(JSON.parse(body).title);
+  }
 });
-
